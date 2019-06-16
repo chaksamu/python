43190840aedc8808cc1de5e5cec1265ac0c78f90
chaksamu <45867824+chaksamu@users.noreply.github.com>
2019-06-16 11:53:34 +0530

    conditionalsteps

---
+++ conditionalsteps.py
@@ -0,0 +1,13 @@
+def main():
+    x = int(input('Enter the First Number: '))
+    y = int(input('Enter the Second Number: '))
+    print( x , y )
+    if x == y:
+        print("Both are same")
+    elif x > y:
+        print("X is bigger")
+    else:
+        print("Y is bigger")
+
+
+main()
